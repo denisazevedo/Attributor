cdc1de335922cc038377e31f4698810d2e89243e
Denis C de Azevedo <denis.azevedo@gmail.com>
2014-01-23 11:17:25 -0200

    add code from teacher

--- Attributor/AttributorViewController.m
+++ Attributor/AttributorViewController.m
@@ -19,17 +19,22 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-    
-    //Get the mutable attributed string from the button
-    NSMutableAttributedString *title =
+    [self setupOutlineButton];
+}
+
+- (void)setupOutlineButton {
+    if (self.outlineButton.currentTitle) {
+        //Get the mutable attributed string from the button
+        NSMutableAttributedString *title =
         [[NSMutableAttributedString alloc] initWithString:self.outlineButton.currentTitle];
-    //Set its attributes
-    [title setAttributes:@{ NSStrokeWidthAttributeName: @3,
-                            NSStrokeColorAttributeName: self.outlineButton.tintColor }
-                   range:NSMakeRange(0, [title length])];
-    //Set these modified attributes (mutable) to the button
-    [self.outlineButton setAttributedTitle:title forState:UIControlStateNormal];
+        //Set its attributes
+        [title setAttributes:@{ NSStrokeWidthAttributeName: @3,
+                                NSStrokeColorAttributeName: self.outlineButton.tintColor }
+                       range:NSMakeRange(0, [title length])];
+        //Set these modified attributes (mutable) to the button
+        [self.outlineButton setAttributedTitle:title forState:UIControlStateNormal];
+    }
+
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -48,7 +53,7 @@
                                                   object:nil];
 }
 
-- (void)preferredFontsChanged:(NSNotificationCenter *)notification {
+- (void)preferredFontsChanged:(NSNotification *)notification {
     [self usePreferredFonts];
 }
 
