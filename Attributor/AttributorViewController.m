8df8b548e1d48751f0d1d3ed317161514a641cc4
Denis C de Azevedo <denis.azevedo@gmail.com>
2014-01-23 09:49:18 -0200

    add stroke to outlineButton on viewDidLoad

--- Attributor/AttributorViewController.m
+++ Attributor/AttributorViewController.m
@@ -12,9 +12,25 @@
 
 @property (weak, nonatomic) IBOutlet UITextView *body;
 @property (weak, nonatomic) IBOutlet UILabel *headLine;
+@property (weak, nonatomic) IBOutlet UIButton *outlineButton;
 @end
 
 @implementation AttributorViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    
+    //Get the mutable attributed string from the button
+    NSMutableAttributedString *title =
+        [[NSMutableAttributedString alloc] initWithString:self.outlineButton.currentTitle];
+    //Set its attributes
+    [title setAttributes:@{ NSStrokeWidthAttributeName: @3,
+                            NSStrokeColorAttributeName: self.outlineButton.tintColor }
+                   range:NSMakeRange(0, [title length])];
+    //Set these modified attributes (mutable) to the button
+    [self.outlineButton setAttributedTitle:title forState:UIControlStateNormal];
+}
 
 - (IBAction)changeBodySelectionColorToMatchBackgroundOfButton:(UIButton *)sender {
     [self.body.textStorage addAttribute:NSForegroundColorAttributeName
@@ -31,11 +47,6 @@
 - (IBAction)unoutlineBodySelection {
     [self.body.textStorage removeAttribute:NSStrokeWidthAttributeName
                                      range:self.body.selectedRange];
-}
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
 }
 
 @end
